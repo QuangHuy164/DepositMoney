4fb9b4c6135cd913c8a68477d6398a7baaf299ec
unknown <nghuy164@gmail.com>
2024-09-04 03:53:48 +0300

    update

--- AI.js
+++ AI.js
@@ -1,17 +1,29 @@
-const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")();
 
 const deposit = () => {
-    while (true) {
-    const depositAmount = prompt('Enter a deposit amount: ');
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount: ");
     const numberDepositAmount = parseFloat(depositAmount);
 
-    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0){
-        console.log('Invalid deposit amount, try again')
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount, try again");
     } else {
-        return numberDepositAmount;
+      return numberDepositAmount;
     }
-}
+  }
 };
 
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3 ) {
+      console.log("Invalid number of lines, try again");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
 const depositAmount = deposit();
 console.log(depositAmount);
